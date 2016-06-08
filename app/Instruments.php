d7d04eb4e982ce8cfc9b4ee44f2273aedb490eab
Wei Fang <wei@ralphandco.com>
2016-06-08 12:24:03 -0400

    add pagination

--- app/Instruments.php
+++ app/Instruments.php
@@ -3,12 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator as Paginator;
 
 class Instruments extends Model
 {
     protected $table = 'instruments';
     
     public function fetchAll() {
-        return Instruments::all();
+        return Instruments::where('status', 'A')->paginate(8);
     }
 }
--- resources/views/items.blade.php
+++ resources/views/items.blade.php
@@ -21,85 +21,28 @@
     <!-- /.row -->
 
     <!-- Projects Row -->
+    <?php $nums = count($items); ?>
+    @for ($j = 0; $j < ceil($nums / 3); $j++)
     <div class="row">
-        @for ($i = 0; $i < 3; $i++)
-            <div class="col-md-4 img-portfolio">
-                <a href="/item">
-                    <img class="img-responsive img-hover" src="http://placehold.it/700x400" alt="">
-                </a>
-                {{ $items[$i]['image'] }}
-                <h3>
-                    <a href="/item">{{ $items[$i]['name'] }}</a>
-                </h3>
-                <p>{{ $items[$i]['introduction'] }}</p>
-            </div>
+        @for ($i = 0; $i < 3 && $i < $nums; $i++, $nums--)
+        <div class="col-md-4 img-portfolio">
+            <a href="/item">
+                <img class="img-responsive img-hover" src="http://placehold.it/700x400" alt="">
+            </a>
+            {{ $items[$i]['image'] }}
+            <h3>
+                <a href="/item">{{ $items[$i]['name'] }}</a>
+            </h3>
+            <p>{{ $items[$i]['introduction'] }}</p>
+        </div>
         @endfor
     </div>
+    @endfor
     <!-- /.row -->
-
-    <!-- Projects Row -->
-    <div class="row">
-        @for ($i = 3; $i < 6; $i++)
-            <div class="col-md-4 img-portfolio">
-                <a href="/item">
-                    <img class="img-responsive img-hover" src="http://placehold.it/700x400" alt="">
-                </a>
-                {{ $items[$i]['image'] }}
-                <h3>
-                    <a href="/item">{{ $items[$i]['name'] }}</a>
-                </h3>
-                <p>{{ $items[$i]['introduction'] }}</p>
-            </div>
-        @endfor
-    </div>
-
-    <!-- Projects Row -->
-    <div class="row">
-        @for ($i = 6; $i < 9; $i++)
-            <div class="col-md-4 img-portfolio">
-                <a href="/item">
-                    <img class="img-responsive img-hover" src="http://placehold.it/700x400" alt="">
-                </a>
-                {{ $items[$i]['image'] }}
-                <h3>
-                    <a href="/item">{{ $items[$i]['name'] }}</a>
-                </h3>
-                <p>{{ $items[$i]['introduction'] }}</p>
-            </div>
-        @endfor
-    </div>
-    <!-- /.row -->
-
     <hr>
 
     <!-- Pagination -->
-    <div class="row text-center">
-        <div class="col-lg-12">
-            <ul class="pagination">
-                <li>
-                    <a href="#">&laquo;</a>
-                </li>
-                <li class="active">
-                    <a href="#">1</a>
-                </li>
-                <li>
-                    <a href="#">2</a>
-                </li>
-                <li>
-                    <a href="#">3</a>
-                </li>
-                <li>
-                    <a href="#">4</a>
-                </li>
-                <li>
-                    <a href="#">5</a>
-                </li>
-                <li>
-                    <a href="#">&raquo;</a>
-                </li>
-            </ul>
-        </div>
-    </div>
+    @include('pagination.default', ['paginator' => $items])
     <!-- /.row -->
 
     <hr>
