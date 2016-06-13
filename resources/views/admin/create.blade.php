3bed16c9a53a929b8e287d8e86c8f0533d5db677
Wei Fang <wei@ralphandco.com>
2016-06-13 12:55:48 -0400

    create user

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -7,60 +7,70 @@
     @include('admin.navbar')
 
     <div id="page-wrapper">
-        <div class="col-lg-8">
-            <div class="panel-heading"><h2>创建新用户</h2></div>
-            <div class="panel-body">
-                <div class="row">
-                    <form  class="form-horizontal" role="form" method="post" action="{{ url('/admin/') }}">
-                        {!! csrf_field() !!}
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header"><i class="fa fa-users"></i> 用户管理</h2>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h2>创建新用户</h2></div>
+                    <div class="panel-body">
+                        <form  class="form-horizontal" role="form" method="post" action="{{ url('/admin/') }}">
+                            {!! csrf_field() !!}
 
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label">用户名</label>
-                            <div class="col-lg-6">
-                                <input class="form-control" type="text" name="user-name" value="" />
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">用户名</label>
+                                <div class="col-lg-6">
+                                    <input class="form-control" type="text" name="user-name" value="" />
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label">电子邮箱</label>
-                            <div class="col-lg-6">
-                                <input class="form-control" type="text" name="user-email" value="" />
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">电子邮箱</label>
+                                <div class="col-lg-6">
+                                    <input class="form-control" type="text" name="user-email" value="" />
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label">设置密码</label>
-                            <div class="col-lg-6">
-                                <input type="password" class="form-control" name="password">
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">密 码</label>
+                                <div class="col-lg-6">
+                                    <input type="password" class="form-control" name="password">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label">确认密码</label>
-                            <div class="col-lg-6">
-                                <input type="password" class="form-control" name="password">
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">确认密码</label>
+                                <div class="col-lg-6">
+                                    <input type="password" class="form-control" name="password">
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label">权 限</label>
-                            <label class="radio-inline">
-                                <input type="radio" name="role" id="radio1" value="N" checked>未认证会员
-                            </label>
-                            <label class="radio-inline">
-                                <input type="radio" name="role" id="radio2" value="A">认证会员
-                            </label>
-                            <label class="radio-inline">
-                                <input type="radio" name="role" id="radio3" value="V">管理员
-                            </label>
-                        </div>
+                            <div class="form-group">
+                                <label class="col-lg-2 control-label">权 限</label>
+                                <label class="col-lg-1 control-label"></label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="role" id="radio1" value="N" checked>未认证会员
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="role" id="radio2" value="A">认证会员
+                                </label>
+                                <label class="radio-inline">
+                                    <input type="radio" name="role" id="radio3" value="V">管理员
+                                </label>
+                            </div>
 
-                        <div class="form-group">
-                            <label class="col-lg-2 control-label"></label>
-                            <button type="submit" class="btn btn-default">更 新</button>
-                            <button type="reset" class="btn btn-default">重 置</button>
-                        </div>
-                    </form>
+                            <div class="form-group">
+                                <label class="col-lg-4 control-label"></label>
+                                <button type="submit" class="btn btn-default">创 建</button>
+                                <button type="reset" class="btn btn-default">重 置</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/instruments/edit.blade.php
+++ resources/views/instruments/edit.blade.php
@@ -7,45 +7,54 @@
     @include('admin.navbar')
 
     <div id="page-wrapper">
-        <div class="panel-heading"><h2>更新仪器信息</h2></div>
-        <div class="panel-body">
-            <div class="row">
-                <div class="col-lg-6">
-                    <form role="form" method="post" action="{{ url('/items/' . $item->id) }}" enctype="multipart/form-data">
-                        {!! csrf_field() !!}
-                        
-                        <input type="hidden" name="_method" value="PUT">
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header"><i class="fa fa-gears"></i> 设备管理</h2>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading"><h2>更新仪器信息</h2></div>
+                    <div class="panel-body">
+                        <form role="form" method="post" action="{{ url('/items/' . $item->id) }}" enctype="multipart/form-data">
+                            {!! csrf_field() !!}
 
-                        <div class="form-group">
-                            <label>仪器型号</label>
-                            <input class="form-control" type="text" name="item-name" value="{{ $item->name }}" />
-                        </div>
+                            <input type="hidden" name="_method" value="PUT">
 
-                        <div class="form-group">
-                            <label>设备简介</label>
-                            <textarea class="form-control" name="item-introduction" rows="20">{{ $item->introduction }}</textarea>
-                        </div>
+                            <div class="form-group">
+                                <label>仪器型号</label>
+                                <input class="form-control" type="text" name="item-name" value="{{ $item->name }}" />
+                            </div>
 
-                        <div class="form-group">
-                            <label>图 片</label><br>
-                            <img src="{{ $item->image }}"/>
-                            <input type="file" name='item-image'>
-                        </div>
+                            <div class="form-group">
+                                <label>设备简介</label>
+                                <textarea class="form-control" name="item-introduction" rows="15">{{ $item->introduction }}</textarea>
+                            </div>
 
-                        <div class="form-group">
-                            <label>入庫日期</label>
-                            <p class="form-control-static">{{ $item->created_at }}</p>
-                        </div>
+                            <div class="form-group">
+                                <label>图 片</label><br>
+                                <img src="{{ $item->image }}"/>
+                                <input type="file" name='item-image'>
+                            </div>
 
-                        <div class="checkbox">
-                            <label>
-                                <input type="checkbox" <?php if ($item->deleted_at !== NULL) echo "checked"; ?> name="item-delete" /> 放入回收站
-                            </label>
-                        </div>
+                            <div class="form-group">
+                                <label>入庫日期</label>
+                                <p class="form-control-static">{{ $item->created_at }}</p>
+                            </div>
 
-                        <button type="submit" class="btn btn-default">更 新</button>
-                        <button type="reset" class="btn btn-default">重 置</button>
-                    </form>
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox" <?php if ($item->deleted_at !== NULL) echo "checked"; ?> name="item-delete" /> 放入回收站
+                                </label>
+                            </div>
+
+                            <button type="submit" class="btn btn-default">更 新</button>
+                            <button type="reset" class="btn btn-default">重 置</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
