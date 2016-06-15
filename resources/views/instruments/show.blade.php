e62d627dd769a56c41382b901f20ef90f6a1b1f3
Wei Fang <wei@ralphandco.com>
2016-06-15 16:43:54 -0400

    updates

--- resources/views/instruments/show.blade.php
+++ resources/views/instruments/show.blade.php
@@ -9,16 +9,17 @@
     <div class="row">
         <div class="col-lg-12">
             <h1 class="page-header"> 仪器设备
-                <small>{{ $item->name }}</small>
-                @if (Auth::user()->role == 'V')
-                <a class="pull-right" href="/items/{{ $item->id }}/edit"><button type="button" class="btn btn-outline btn-info">编辑此仪器</button></a>
-                @endif
+                <small>{{ $item->name }} </small>
             </h1>
             <ol class="breadcrumb">
-                <li><a href="/">首 页</a>
-                </li>
-                <li class="active"> 仪器设备</li>
+                <li><a href="/">首 页</a></li>
+                <li><a href="/items">仪器设备</a></li>
+                <li class="active">{{ $item->name }}</li>
+                @if (Auth::user()->role == 'V')
+                <a class="pull-right" href="/items/{{ $item->id }}/edit"> <i class="fa fa-pencil"></i>编辑此仪器 </a>
+                @endif
             </ol>
+
         </div>
     </div>
     <!-- /.row -->
@@ -27,35 +28,35 @@
     <div class="row">
 
         <div class="col-md-8">
-<!--            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                 Indicators 
-                <ol class="carousel-indicators">
-                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                </ol>
-
-                 Wrapper for slides 
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <img class="img-responsive" src="http://placehold.it/750x500" alt="">
-                    </div>
-                    <div class="item">
-                        <img class="img-responsive" src="http://placehold.it/750x500" alt="">
-                    </div>
-                    <div class="item">
-                        <img class="img-responsive" src="http://placehold.it/750x500" alt="">
-                    </div>
-                </div>
-
-                 Controls 
-                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
-                </a>
-                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
-                </a>
-            </div>-->
+            <!--            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                             Indicators 
+                            <ol class="carousel-indicators">
+                                <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                            </ol>
+            
+                             Wrapper for slides 
+                            <div class="carousel-inner">
+                                <div class="item active">
+                                    <img class="img-responsive" src="http://placehold.it/750x500" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="img-responsive" src="http://placehold.it/750x500" alt="">
+                                </div>
+                                <div class="item">
+                                    <img class="img-responsive" src="http://placehold.it/750x500" alt="">
+                                </div>
+                            </div>
+            
+                             Controls 
+                            <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left"></span>
+                            </a>
+                            <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right"></span>
+                            </a>
+                        </div>-->
             <img class="img-responsive" src="{{ $item->image }}" />
         </div>
 
@@ -80,13 +81,13 @@
             <h3 class="page-header">类似的仪器设备</h3>
         </div>
         @foreach ($related_items as $related_items)
-        
-<!--        <div class="col-sm-3 col-xs-6">
-            <a href="/items/{{ $related_items->id }}">
-                <img class="img-responsive img-hover img-related" src="{{ $related_items->image }}" style="width: 262.5; height: 157.5 " alt="">
-            </a>
-        </div>-->
-        
+
+        <!--        <div class="col-sm-3 col-xs-6">
+                    <a href="/items/{{ $related_items->id }}">
+                        <img class="img-responsive img-hover img-related" src="{{ $related_items->image }}" style="width: 262.5; height: 157.5 " alt="">
+                    </a>
+                </div>-->
+
         @endforeach
 
         <div class="col-sm-3 col-xs-6">
@@ -94,7 +95,7 @@
                 <img class="img-responsive img-hover img-related" src="http://placehold.it/500x300" alt="">
             </a>
         </div>
-        
+
         <div class="col-sm-3 col-xs-6">
             <a href="#">
                 <img class="img-responsive img-hover img-related" src="http://placehold.it/500x300" alt="">
