7810cda099aa1cb03b3c2abd32318390a2919d1a
Wei Fang <wei@ralphandco.com>
2016-06-21 10:09:46 -0400

    updates

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -57,10 +57,10 @@
                     <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level">
                         <li>
-                            <a href="blank.html">Blank Page</a>
+                            <a href="#">Blank Page</a>
                         </li>
                         <li>
-                            <a href="login.html">Login Page</a>
+                            <a href="#">Login Page</a>
                         </li>
                     </ul>
                     <!-- /.nav-second-level -->
