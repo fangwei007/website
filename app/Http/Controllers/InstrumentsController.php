4215d63e0b6c28e620972eb18f157e3e471464f4
Wei Fang <wei@ralphandco.com>
2016-07-13 18:01:47 -0400

    single page shows same type items

--- app/Http/Controllers/InstrumentsController.php
+++ app/Http/Controllers/InstrumentsController.php
@@ -102,7 +102,9 @@ class InstrumentsController extends Controller {
      */
     public function show($id) {
         $item = Instruments::where('id', $id)->first();
-        $related_items = Instruments::all()->where('type', $item->type)->except($id)->random(4);
+        $total = Instruments::all()->where('type', $item->type)->except($id)->count();
+        if ($total < 4) $related_items = Instruments::all()->where('type', $item->type)->except($id)->random($total);
+        else $related_items = Instruments::all()->where('type', $item->type)->except($id)->random(4);
         return view('instruments.show', ['item' => $item, 'related_items' => $related_items]);
     }
 
