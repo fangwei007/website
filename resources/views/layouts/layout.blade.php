7de254e628bd3e4bdb69548f484c61895325ff3d
Wei Fang <wei@ralphandco.com>
2016-07-14 11:55:26 -0400

    change topnav

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -79,6 +79,8 @@
                                 <li <?php echo Request::is('about') ? 'class="active"' : ''; ?>><a href="/about">关于我们</a></li>
                                 @if (!Auth::guest() && (Auth::user()->isAdmin() || Auth::user()->isMember()))
                                 <li <?php echo Request::is('items') ? 'class="active"' : ''; ?>><a href="/items">产品详情 <i class="fa fa-unlock"></i></a></li>
+                                @else
+                                <li <?php echo Request::is('items') ? 'class="active"' : ''; ?>><a href="/items">产品详情 <i class="fa fa-lock"></i></a></li>
                                 @endif
                                 <li <?php echo Request::is('contact') ? 'class="active"' : ''; ?>><a href="/contact">联系我们</a></li>
                                 <!-- Authentication Links -->
