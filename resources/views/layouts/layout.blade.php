481a5fef30541491f210a92c7aa4dd5d5accffa1
Wei Fang <wei@ralphandco.com>
2016-07-19 17:59:53 -0400

    change logo and header color

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -70,7 +70,7 @@
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
-                            <a class="navbar-brand" href="/"><img src="/images/logo3.png" alt="logo"></a>
+                            <a class="navbar-brand" href="/"><img src="/images/logo7.png" alt="logo"></a>
                         </div>
 
                         <div class="collapse navbar-collapse navbar-right">
