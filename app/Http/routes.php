29a5d46a199445f818fb8e1ba2550fd702a2f93d
Wei Fang <wei@ralphandco.com>
2017-08-09 17:36:58 -0400

    rm some routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -63,24 +63,4 @@ Route::group(["prefix" => "en"], function() {
     Route::get('/contact', function () {
         return view('contact');
     });
-
-    Route::get('/trash', 'AdminController@trash');
-    Route::get('/trash/user/{id}/restore', 'AdminController@restore');
-    Route::get('/trash/user/{id}/perm-delete', 'AdminController@permDestroy');
-    Route::get('/trash/item/{id}/restore', 'InstrumentsController@restore');
-    Route::get('/trash/item/{id}/perm-delete', 'InstrumentsController@permDestroy');
-    Route::resource('items', 'InstrumentsController');
-    Route::resource('admin', 'AdminController');
-    Route::get('/admin/{id}/delete', 'AdminController@destroy');
-    Route::get('/items/{id}/delete', 'InstrumentsController@destroy');
-    Route::post('/contact', 'ContactController@store');
-    Route::get('/user-manage', 'AdminController@userManage');
-    Route::get('/item-manage', 'AdminController@itemManage');
-    Route::get('/msg-manage', 'ContactController@msgManage');
-    Route::get('/msg/{id}', 'ContactController@viewMsg');
-    Route::post('/msg/{id}', 'ContactController@readMsg');
-    Route::get('/msg/{id}/delete', 'ContactController@destroy');
-    Route::get('/trash/msg/{id}/restore', 'ContactController@restore');
-    Route::post('/news', 'AdminController@addNews');
-    Route::get('/news/delete/{id}', 'AdminController@deleteNews');
 });
