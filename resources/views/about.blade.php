8a665fb0b0e515d96a9e7c93d3e0f6462d0ff0d8
Wei Fang <wei@ralphandco.com>
2017-08-11 09:46:03 -0400

    about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -55,6 +55,7 @@ if ($html == null) :
             <div class="col-md-6" style=" margin-top: 20px;">
                 <img class="img-responsive" src="/images/about-footer.jpg" alt="">
             </div>
+            @if ($lang == NULL)
             <div class="col-md-6">
                 <h2><?php echo $lang == NULL ? "公司简介" : "Company Introduction"; ?></h2>
                 <p style="text-indent:2em;">广东康盛生物科技有限公司创建于2003年12月，是经过广东省工商行政管理局及广东省食品药品监督局正式批准注册的集生物技术产品及医疗产品推广、应用、销售和服务于一身的专业化公司。</p>
@@ -63,6 +64,15 @@ if ($html == null) :
                 <p style="text-indent:2em;">我们是德国Pro-Med Instrumente GmbH 外科手术器械及显微器械产品在中国地区的唯一总代理；我们是美国GSC公司SurgiTel手术放大镜、手术头灯及手术摄像系统产品在华中南区七省代理商；我们为客户提供如下诊断试剂：分子生物学实验室的病毒核酸检测、感染性疾病检测、肿瘤基因突变、耐药基因突变及优生优育检测等项目；荧光原位杂交FISH试剂；临床输血基因分型、器官移植、骨髓移植基因配型和临床研究用分子生物学相关项目；移植免疫药物浓度监测及临床生化试剂等项目。</p>
                 <p style="text-indent:2em;">致力于寻求技术领先、 质量上乘的医疗产品，并将之推广至医疗机构和广大患者。公司自成立以来，这些信念和追求从未停止过…</p>
             </div>
+            @else 
+            <div class="col-md-6">
+                <h2><?php echo $lang == NULL ? "公司简介" : "Company Introduction"; ?></h2>
+                <p style="text-indent:2em;">Established in December, 2003, Guangdong Sunny Technologies Inc is a professional company on promoting, selling and marketing products of life science and medical.  We have registered both in Guangdong Provincial Administration for Industry and Guangdong Provincial Food and Drug Administration. </p>
+                <p style="text-indent:2em;">Our mission is providing high quality products and excellent services to medical organizations. We follow one simple concept of "to do everything by heart" and let human beings really get benefit from progress of high-tech through meticulous service to our client.</p>
+                <p style="text-indent:2em;">We are the only distributor of surgical product from Pro-Med Instrumente GmbH in China. We also sell and do promotion activities for SurgiTel products in south China (7 provinces). We provide following products for our clients: virus nucleic acid detection, detection of infection disease, FISH, cancer gene mutation, resistant gene mutation, HLA genotyping and transplantation immunology drug monitoring. </p>
+                <p style="text-indent:2em;">We will never stop seeking innovative technologies, high-quality health care products and extending them to hospital and patients…</p>
+            </div>
+            @endif
         </div>
         <!-- /.row -->
 
